2195da8ea4d6b89ceb2176f9df7164b8c25c48b5
Jiyoun Kang <77395964+kangjiyoun@users.noreply.github.com>
2022-06-10 13:01:44 +0900

    Add files via upload

--- folder/common.js
+++ folder/common.js
@@ -1,19 +1,19 @@
 $(function () {
-  let images = [];
-
-  function preload() {
-    for (let i = 0; i < preload.arguments.lenght; i++) {
-      images[i] = new Image();
-      images[i].src = preload.arguments.src;
-    }
-  }
-
-  preload(
-    "../asset/images/blue_new.png",
-    "../asset/images/opic_blue_bg.png",
-    "../asset/images/opic_title.png",
-    "../asset/images/ts_title.png",
-    "../asset/images/icon_good.png",
-  )
+  // let images = [];
+  //
+  // function preload() {
+  //   for (let i = 0; i < preload.arguments.lenght; i++) {
+  //     images[i] = new Image();
+  //     images[i].src = preload.arguments.src;
+  //   }
+  // }
+  //
+  // preload(
+  //   "../asset/images/blue_new.png",
+  //   "../asset/images/opic_blue_bg.png",
+  //   "../asset/images/opic_title.png",
+  //   "../asset/images/ts_title.png",
+  //   "../asset/images/icon_good.png",
+  // )
 
 });
